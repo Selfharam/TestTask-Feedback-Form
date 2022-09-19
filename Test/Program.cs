e9854b8a5f3f3db1da2f4637b216da84b33cd485
Selfharam <57870930+Selfharam@users.noreply.github.com>
2022-09-19 13:25:24 +0300

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,55 +12,10 @@ namespace Test
         [STAThread]
         static async Task Main()
         {
-            /*
-            //DESKTOP-24S5SPT
-            string connectionString = "Server=DESKTOP-24S5SPT\\MSSQLSERVER1;Database=test;User Id=test;Password=test;Encrypt=False;Trusted_Connection=False";
-            SqlConnection connection = new SqlConnection(connectionString);
-
-           
-                try
-                {
-                // Открываем подключение
-                await connection.OpenAsync();
-                System.Console.WriteLine("Подключение открыто");
-                
-                }
-                catch (SqlException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                
-            }
-                finally
-                {
-                    // если подключение открыто
-                    if (connection.State == ConnectionState.Open)
-                    {
-                    
-                    // закрываем подключение
-                    await connection.CloseAsync();
-                    Console.WriteLine("Подключение закрыто...");
-                    }
-                }
-                Console.WriteLine("Программа завершила работу.");
-                Console.Read();
-            */
-
-            string connectionString = "Server=DESKTOP-24S5SPT\\MSSQLSERVER1;Database=test;User Id=test;Password=test;Encrypt=False;Trusted_Connection=False";
-            //SqlConnection connection = new SqlConnection(connectionString);
-
-
-
-
-
-
-            SqlConnection connection = new SqlConnection(connectionString);
-            //connection.Open();
-
+           string connectionString = "Server=DESKTOP-24S5SPT\\MSSQLSERVER1;Database=test;User Id=test;Password=test;Encrypt=False;Trusted_Connection=False";
             
-
-
-
-
+           SqlConnection connection = new SqlConnection(connectionString);
+            
            ApplicationConfiguration.Initialize();
            Application.Run(new Form1(connection));
         }
